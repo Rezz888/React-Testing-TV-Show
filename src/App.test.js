2642ec91982c7464b159ab0b50bacfdd60575a6b
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2020-09-07 17:13:34 -0400

    MVP

---
+++ src/App.test.js
@@ -0,0 +1,38 @@
+import App from './App';
+import { fetchShow as mockFetchShow } from './api/fetchShow';
+
+import React from 'react';
+import {
+  render,
+  waitFor,
+} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+
+jest.mock('./api/fetchShow');
+
+test('App fetches show and renders it', async () => {
+  mockFetchShow.mockResolvedValueOnce(mockData);
+  const { queryAllByText } = render(<App />);
+  expect(queryAllByText(/fetching data.../i)).toHaveLength(1);
+  await waitFor(() => {
+    expect(queryAllByText(/summary/i)).toHaveLength(1);
+  });
+});
+
+const mockData = {
+  image: { original: "original"},
+  name: "name",
+  summary: "<p>summary</p>",
+  _embedded: {
+      episodes: [{
+          id: "1",
+          image: { medium: "medium_image"},
+          name: "name",
+          season: 3,
+          number: 2,
+          summary: "<p>Summary</p>",
+          runtime: 20
+      }]
+  }
+};
